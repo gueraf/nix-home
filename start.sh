2571c7c71d3a307fb3f1fadf9aac14677354c2f3
Fabian Guera <fabian@odyssey.systems>
2025-08-04 13:49:21 +0100

    Set name of instance.

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-sudo docker run -d -v /home/fabian:/home/fabian/host_fabian gueraf/dev:latest
-
+docker run -d --name fabian-dontkillorelse -v /home/fabian:/home/fabian/host_fabian gueraf/dev:latest

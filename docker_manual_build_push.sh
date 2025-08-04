6efcf8516c9ca5c7f09dc986c5f8afd9da44da6a
fabian <fabian.guera@gmail.com>
2025-08-04 14:28:53 +0200

    Push without root.

--- docker_manual_build_push.sh
+++ docker_manual_build_push.sh
@@ -5,6 +5,6 @@ echo "Building Docker image..."
 sudo docker build -t gueraf/dev:latest .
 
 echo "Pushing Docker image to Docker Hub..."
-sudo docker push gueraf/dev:latest
+docker push gueraf/dev:latest
 
 echo "Done! Image gueraf/dev:latest has been built and pushed."

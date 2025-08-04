32a5a250fc091bc2c4edd2d9061079ef127f81a8
Fabian Guera <fabian@odyssey.systems>
2025-08-04 11:38:24 +0100

    Build and push manually with sudo.

--- docker_manual_build_push.sh
+++ docker_manual_build_push.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "Building Docker image..."
-docker build -t gueraf/dev:latest .
+sudo docker build -t gueraf/dev:latest .
 
 echo "Pushing Docker image to Docker Hub..."
-docker push gueraf/dev:latest
+sudo docker push gueraf/dev:latest
 
 echo "Done! Image gueraf/dev:latest has been built and pushed."

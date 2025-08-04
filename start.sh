6cbfe37e64aae208202f8e87a99c6cf68e556a17
Fabian Guera <fabian.guera@gmail.com>
2025-08-04 15:48:54 +0000

    Start with gpu.

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d --name fabian-dontkillorelse -v /home/fabian:/home/fabian/host_fabian gueraf/dev:latest
+docker run -d --name fabian-dontkillorelse -v /home/fabian:/home/fabian/host_fabian --runtime=nvidia --gpus all gueraf/dev:latest

9db9405c28b3a62ae3eaa1c4724360728c4c2dba
Fabian Guera <fabian@odyssey.systems>
2025-08-04 13:44:31 +0100

    Add build user to docker group if not there yet.

--- docker_manual_build_push.sh
+++ docker_manual_build_push.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+# Check if fabian is not in the docker group
+if ! groups fabian | grep -q "docker"; then
+    echo "Adding fabian to the docker group..."
+    usermod -aG docker fabian
+    echo "Running as fabian..."
+    su fabian -
+fi
+
 echo "Building Docker image..."
-sudo docker build -t gueraf/dev:latest .
+docker build -t gueraf/dev:latest .
 
 echo "Pushing Docker image to Docker Hub..."
 docker push gueraf/dev:latest

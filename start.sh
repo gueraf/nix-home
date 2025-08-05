4890769a43e6e5b9a4a1d55005ac3f47e98940b9
Fabian <gueraf@users.noreply.github.com>
2025-08-05 15:56:37 +0200

    Pull before start and run priviliged.

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-docker run -d --name fabian-dontkillorelse -v /home/fabian:/home/fabian/host_fabian --runtime=nvidia --gpus all gueraf/dev:latest
+docker pull gueraf/dev:latest && \
+docker run -d --name fabian-dontkillorelse -v /home/fabian:/home/fabian/host_fabian --privileged --runtime=nvidia --gpus all gueraf/dev:latest

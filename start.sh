7cc920a332e847845e7d3069fa4b312971ee4761
Fabian <gueraf@users.noreply.github.com>
2025-08-07 19:54:30 +0200

    /mnt/data/local/fabian:/home/fabian/host_fabian

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 docker pull gueraf/dev:latest && \
-docker run -d --name fabian-dontkillorelse -v /home/fabian:/home/fabian/host_fabian --privileged --runtime=nvidia --gpus all gueraf/dev:latest
+docker run -d --name fabian-dontkillorelse -v /mnt/data/local/fabian:/home/fabian/host_fabian --privileged --runtime=nvidia --gpus all gueraf/dev:latest

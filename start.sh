8a42f2d606ecc061758916344c0f2ad7b492bc32
Fabian Guera <fabian@odyssey.systems>
2025-08-27 09:46:11 +0100

    Restart docker if it's running.

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 docker pull gueraf/dev:latest && \
+docker rm -f fabian-dontkillorelse && \
 docker run -d --name fabian-dontkillorelse -v /mnt/data/local/fabian:/home/fabian/host_fabian --privileged --runtime=nvidia --gpus all gueraf/dev:latest

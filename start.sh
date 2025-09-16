0d330202efabc040e4710b7c3502bbb7ce0e989e
Fabian Guera <fabian@odyssey.systems>
2025-09-16 03:55:25 +0100

    Optional removal of existing container.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker pull gueraf/dev:latest && \
-docker rm -f fabian-dontkillorelse && \
+docker rm -f fabian-dontkillorelse || true && \
 docker run -d --name fabian-dontkillorelse -v /mnt/data/local/fabian:/home/fabian/host_fabian --privileged --runtime=nvidia --gpus all --shm-size 8G gueraf/dev:latest
